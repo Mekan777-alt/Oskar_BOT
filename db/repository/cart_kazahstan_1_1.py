250cc9bda211db998402c6d5c8c4f3c399361a12
Mekan777-alt <mekan2678@gmail.com>
2024-07-02 11:37:40 +0300

    fix insert data add text from sqlalchemy

--- db/repository/cart_kazahstan_1_1.py
+++ db/repository/cart_kazahstan_1_1.py
@@ -1,10 +1,11 @@
 from config import session
+from sqlalchemy import text
 
 
 async def insert_data(data):
-    query = """INSERT INTO admin_panel_requestuser 
+    query = text("""INSERT INTO admin_panel_requestuser 
                (first_name, last_name, patronymic, phone_number, IIN) 
-               VALUES (:first_name, :last_name, :patronymic, :phone_number, :IIN)"""
+               VALUES (:first_name, :last_name, :patronymic, :phone_number, :IIN)""")
     await session.execute(query,
                           {"first_name": data['first_name'],
                            "last_name": data['last_name'],
