7e6537a063ef5a82ab61ceab4d150e9167ac0be0
Mekan777-alt <mekan2678@gmail.com>
2024-07-02 12:17:36 +0300

    defau;t datetime

--- db/repository/cart_kazahstan_1_1.py
+++ db/repository/cart_kazahstan_1_1.py
@@ -1,14 +1,17 @@
+from datetime import datetime
+
 from config import session
 from sqlalchemy import text
 
 
 async def insert_data(data):
     query = text("""INSERT INTO admin_panel_requestuser 
-               (first_name, last_name, patronymic, phone_number, iin) 
-               VALUES (:first_name, :last_name, :patronymic, :phone_number, :IIN)""")
+                   (first_name, last_name, patronymic, phone_number, iin, created_at) 
+                   VALUES (:first_name, :last_name, :patronymic, :phone_number, :iin, :created_at)""")
     await session.execute(query,
                           {"first_name": data['first_name'],
                            "last_name": data['last_name'],
                            "patronymic": data['patronymic'],
                            "phone_number": data['phone_number'],
-                           "IIN": data['IIN']})
+                           "iin": data['IIN'],
+                           "created_at": datetime.now()})
